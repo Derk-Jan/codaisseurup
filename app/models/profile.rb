0ecde9c3f8699d5d472af82939ad94d202194b71
derk-jan <derk.vandervaart@gmail.com>
2017-03-16 13:21:06 +0100

    added model tests

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,13 +1,13 @@
 class Profile < ApplicationRecord
-   validates :first_name, presence: true
-   validates :last_name, presence: true
-   validates :bio, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :bio, presence: true
 
-    def full_name
-      "#{first_name} #{last_name}"
-    end
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 
   def self.by_letter(letter)
-     where("first_name LIKE ?", "#{letter}%").order(:first_name)
-   end
+    where("first_name LIKE ?", "#{letter}%").order(:first_name)
   end
+end
